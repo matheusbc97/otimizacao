931033d7852568946e832356054f04e40fa9182e
Matheus Botelho <39282759+matheusbc97@users.noreply.github.com>
2022-05-04 10:59:14 -0300

    Update mini_photoshop.py

--- mini_photshop/mini_photoshop.py
+++ mini_photshop/mini_photoshop.py
@@ -4,35 +4,34 @@ import os
 from matplotlib import pyplot as plt
 import numpy as np
 
-imagem = cv2.imread(os.path.expanduser( "~/Downloads/floresta.png"))
-
 def loadImage():
+    imagem = cv2.imread(os.path.expanduser( "~/Downloads/projeto/lena.png"))
     cv2.imshow('Image', imagem)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     print("Carregamento de imagem feito com sucesso!\n")
-    showOptions()
+    showOptions(imagem)
 
-def convertImageColor(conversionType):
+def convertImageColor(conversionType, imagem):
     if(conversionType == 'grey'):
-        gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("Gray", gray)
+        imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Gray", imagem)
     elif(conversionType == 'hsv'):
-        hsv = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
-        cv2.imshow("hsv", hsv)
+        imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
+        cv2.imshow("hsv", imagem)
     elif(conversionType == 'hls'):
-        hls = cv2.cvtColor(imagem, cv2.COLOR_BGR2HLS)
-        cv2.imshow("hls", hls)
+        imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2HLS)
+        cv2.imshow("hls", imagem)
     elif(conversionType == 'YCrCB'):
-        YCrCB = cv2.cvtColor(imagem, cv2.COLOR_BGR2YCrCb)
-        cv2.imshow("YCrCB", YCrCB)
+        imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2YCrCb)
+        cv2.imshow("YCrCB", imagem)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     print("Conversão feita com sucesso!\n")
-    showOptions()
+    showOptions(imagem)
 
-def showImageColorsConvertionsOptions():
+def showImageColorsConvertionsOptions(imagem):
     print("Escolha uma das opção abaixo de conversão:")
     print("[1] - Tons de cinza")
     print("[2] - HSV")
@@ -41,15 +40,15 @@ def showImageColorsConvertionsOptions():
     opcao = input()
 
     if (opcao == "1"):
-        convertImageColor('grey')
+        convertImageColor('grey', imagem)
     elif (opcao == "2"):
-        convertImageColor('hsv')
+        convertImageColor('hsv', imagem)
     elif (opcao == "3"):
-        convertImageColor('hls')
+        convertImageColor('hls', imagem)
     elif (opcao == "4"):
-        convertImageColor('YCrCB')
+        convertImageColor('YCrCB', imagem)
 
-def showHistogram(histogramType):
+def showHistogram(histogramType, imagem):
     plt.figure()
   
     plt.xlabel("Itensidade")
@@ -74,37 +73,37 @@ def showHistogram(histogramType):
     plt.show()
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    showOptions()
+    showOptions(imagem)
 
-def showHistogramsOptions():
+def showHistogramsOptions(imagem):
     print("Escolha um dos histograms abaixo:")
     print("[1] - Histograma da imagem monocromática")
     print("[2] - Histograma da imagem original RGB")
     opcao = input()
 
     if (opcao == "1"):
-        showHistogram('mono')
+        showHistogram('mono',imagem)
     elif (opcao == "2"):
-        showHistogram('rgb')
+        showHistogram('rgb', imagem)
 
-def highlightByHistogram():
+def highlightByHistogram(imagem):
     # Converte imagem para tons de cinza
-    imagem2 = cv2.cvtColor(imagem,cv2.COLOR_BGR2GRAY)
+    imagem = cv2.cvtColor(imagem,cv2.COLOR_BGR2GRAY)
 
     # Realce da imagem usando equalização de histogramas
-    imagemEq = cv2.equalizeHist(imagem2)
+    imagemEq = cv2.equalizeHist(imagem)
 
     # Empilha imagens lado a lado
-    resultado = np.hstack((imagem2, imagemEq))
+    imagem = np.hstack((imagem, imagemEq))
 
     #Mostra resultados
-    cv2.imshow('Imagem original - imagem equalizada',resultado)
+    cv2.imshow('Imagem original - imagem equalizada',imagem)
     cv2.waitKey(0)
 
     cv2.destroyAllWindows()
-    showOptions()
+    showOptions(imagem)
 
-def showFiltersOptions():
+def showFiltersOptions(imagem):
     print("Escolha um dos histograms abaixo:")
     print("[1] - Média")
     print("[2] - Mediana")
@@ -114,46 +113,48 @@ def showFiltersOptions():
     opcao = input()
 
     if (opcao == "1"):
-        media = np.hstack([
+        imagem = np.hstack([
             cv2.blur(imagem,(3,3)),
             cv2.blur(imagem,(5,5)),
             cv2.blur(imagem,(7,7))
         ])
-        cv2.imshow('Filtro média',media)
+        cv2.imshow('Filtro média',imagem)
         cv2.waitKey(0)
     elif (opcao == "2"):
         # filtro mediana com diferentes máscaras
         # Empilha resultados lado a lado
-        mediana = np.hstack([
+        imagem = np.hstack([
             cv2.medianBlur(imagem,3),
             cv2.medianBlur(imagem,5),
             cv2.medianBlur(imagem,7)
         ])
 
-        cv2.imshow('Filtro mediana',mediana)
+        cv2.imshow('Filtro mediana',imagem)
         cv2.waitKey(0)
     elif (opcao == "4"):
         # filtro gaussiano com diferentes máscaras
         # Empilha resultados lado a lado
-        gauss= np.hstack([
+        imagem= np.hstack([
         cv2.GaussianBlur(imagem,(3,3),0),
         cv2.GaussianBlur(imagem,(5,5),0),
         cv2.GaussianBlur(imagem,(7,7),0)])
 
-        cv2.imshow('Filtro gaussiano',gauss)
+        cv2.imshow('Filtro gaussiano',imagem)
         cv2.waitKey(0)
-    showOptions()
+    showOptions(imagem)
 
+def saveImage(imagem):
+    cv2.imwrite(os.path.expanduser( "~/Downloads/projeto/outra-lena.png"), imagem)
 
-def showOptions():
+def showOptions(imagem):
     print("Escolha uma das perações abaixo:")
     print("[1] - Carregar uma imagem")
-    print("[2] - Binarizar uma imagem")
+    #print("[2] - Binarizar uma imagem")
     print("[3] - Conversão da imagem para outro sistema de cor")
     print("[4] - Mostrar o histograma da imagem")
     print("[5] - Realce da imagem por meio de equalização de histogramas")
     print("[6] - Aplicar filtros")
-    print("[7] - Detectar bordas com o método de Canny")
+    #print("[7] - Detectar bordas com o método de Canny")
     print("[8] - Salvar a imagem processada em um novo arquivo")
     print("[9] - Sair da aplicação")
     opcao = input()
@@ -161,14 +162,14 @@ def showOptions():
     if (opcao == "1"):
         loadImage()
     elif (opcao == "3"):
-        showImageColorsConvertionsOptions()
+        showImageColorsConvertionsOptions(imagem)
     elif (opcao == "4"):
-        showHistogramsOptions()
+        showHistogramsOptions(imagem)
     elif (opcao == "5"):
-        highlightByHistogram()
+        highlightByHistogram(imagem)
     elif (opcao == "6"):
-        showFiltersOptions()
-        
+        showFiltersOptions(imagem)
+    elif (opcao == "8"):
+        saveImage(imagem)
 
-
-showOptions()
+showOptions('')
